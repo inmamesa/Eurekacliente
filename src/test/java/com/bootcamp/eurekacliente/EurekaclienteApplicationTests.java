7abb08c53d096276011a81fba8c5ef7756e95da7
inmamesa <inmamesa98@gmail.com>
2022-02-07 12:47:04 +0100

    primer commit

---
+++ src/test/java/com/bootcamp/eurekacliente/EurekaclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.eurekacliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
